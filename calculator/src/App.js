bf6a43d6f306e043b3c7e348875989745f30970f
filthyningen <malikliddell@gmail.com>
2023-01-15 14:53:58 -0500

    funciton reducer will call actions to return opperand annd operation.

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,23 +1,51 @@
+import { useReducer } from "react"
 import "./style.css"
 
+const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
+
+
+function reducer(state, { type, payload }) {
+  switch(type) {
+    case ACTIONS.ADD_DIGIT:
+    return {
+      ...state,
+      currentOpperand: `${currentOpperand || ""}${payload.digit}`
+    }
+  }
+}
+
+
 function App() {
+
+  const [{currentOpperand,previousOpperand,operation}, dispatch] = useReducer(reducer, {})
+
+  dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit:1}})
+
   return (
     <div className="calculator-grid">
       
       <div className="output">
-        <div className="previous-opperend"></div>
-        <div className="current-opperend"></div>
+        <div className="previous-opperand">{previousOpperand} {operation}</div>
+        <div className="current-opperand">{currentOpperand}</div>
       </div>
 
       <button className="span-two">AC</button>
       <button >DEL</button>
-      <button >+</button>
+      <button >/</button>
       <button >1</button>
       <button >2</button>
       <button >3</button>
+      <button >*</button>
       <button >4</button>
       <button >5</button>
       <button >6</button>
+      <button >+</button>
       <button >7</button>
       <button >8</button>
       <button >9</button>
