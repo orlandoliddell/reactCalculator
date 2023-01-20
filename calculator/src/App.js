fd2418e8cffc82f5d573f0db5f123336549763df
filthyningen <malikliddell@gmail.com>
2023-01-20 12:38:51 -0500

    case choose operation will add operation tostate appropriately. the evaluate function was created.

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -17,39 +17,65 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
   switch(type) {
     case ACTIONS.ADD_DIGIT:
-      if (payload.digit ==="0" && state.currentOpperand === "0") return state
+      if (payload.digit ==="0" && state.currentOperand === "0") return state
       if (payload.digit === "." && state.currentOperand == null) { 
         return { ...state, 
-          currentOpperand: payload.digit,
+          currentOperand: payload.digit,
         }; 
       } 
-      if (payload.digit === "." && state.currentOperand.includes(".")) { 
-        return { ...state, 
-          currentOpperand: payload.digit,  
-        };
-      } 
       // '.' isn't adding on to current state. clears state and displays the '.'
+      if (payload.digit === "." && state.currentOperand.includes(".")) { 
+        return state
+      } 
     return {
       ...state,
-      currentOpperand: `${state.currentOpperand || ""}${payload.digit}`
+      currentOperand: `${state.currentOperand || ""}${payload.digit}`
     }
-  case ACTIONS.CLEAR:
+
+  case ACTIONS.CHOOSE_OPERATION:
+    if (state.currentOperand == null && state.previousOperand == null) {
+      return state
+    }
+    if (state.previousOperand == null) {
+      return {
+        ...state,
+        operation:payload.operation,
+        previousOperand: state.currentOperand,
+        currentOperand: null,
+      }
+    }
+    return{
+      ...state,
+      previousOperand: evalute(state),
+      operation: payload.operation,
+      currentOperand: null
+    }
+    
+    case ACTIONS.CLEAR:
       return {};
   }
 }
 
 
+function evaluate({ currentOperand,previousOperand,operation }) {
+  const prev = parseFloat(previousOperand)
+  const current = parseFloat(currentOperand)
+  if(isNaN(prev) || isNaN(current)) return ""
+  let computation = ""
+
+}
+
 function App() {
 
-  const [{currentOpperand, previousOpperand, operation}, dispatch] = useReducer(reducer, {})
+  const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {})
 
 
   return (
     <div className="calculator-grid">
       
       <div className="output">
-        <div className="previous-opperand">{previousOpperand} {operation}</div>
-        <div className="current-opperand">{currentOpperand}</div>
+        <div className="previous-opperand">{previousOperand} {operation}</div>
+        <div className="current-opperand">{currentOperand}</div>
       </div>
 
       <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR })}>
